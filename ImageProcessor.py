7ed49e577810b5c9da6cd8256ed8a9510ead9369
Asad Zia <asadzia@gmail.com>
2018-03-20 13:33:59 +0500

    Resize test images and video to the orginial shape after infernce.

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -32,20 +32,22 @@ class ImageProcessor(object):
         logits = self.logits
         keep_prob = self.keep_prob
         image_pl = self.input_image
-        image_shape = self.image_shape
+        requied_shape = self.image_shape
+        original_shape = image.shape
 
-        image = scipy.misc.imresize(image, image_shape)
-            
+        image = scipy.misc.imresize(image, requied_shape)
+
         im_softmax = sess.run(
             [tf.nn.softmax(logits)],
             {keep_prob: 1.0, image_pl: [image]})
         im_softmax = im_softmax[0][:, 1].reshape(
-            image_shape[0], image_shape[1])
+            requied_shape[0], requied_shape[1])
         segmentation = (im_softmax > 0.5).reshape(
-            image_shape[0], image_shape[1], 1)
+            requied_shape[0], requied_shape[1], 1)
         mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
         mask = scipy.misc.toimage(mask, mode="RGBA")
         street_im = scipy.misc.toimage(image)
         street_im.paste(mask, box=None, mask=mask)
 
-        return np.array(street_im)
+        # Restore orginal image shape
+        return scipy.misc.imresize(np.array(street_im), original_shape)
